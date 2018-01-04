6b00e2fa1937acb0801268737ac306f391dea5a5
Jeff Cisneros <cisneros24@gmail.com>
2018-01-04 01:16:53 +0000

    add search option on all campground page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,14 +6,32 @@ var geocoder = require("geocoder");
 
 //Index Route - show all campgrounds
 router.get("/", function(req, res){
-    // get all campground for db
-    Campground.find({}, function(err, allCampgrounds){
-        if(err){
-            console.log(err);
-        } else{
-            res.render("campgrounds/index", {campgrounds: allCampgrounds});
-        }
-    })
+    var noMatch = null;
+    if(req.query.search){
+         const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+         // get all campground for db
+        Campground.find({name: regex}, function(err, allCampgrounds){
+            if(err){
+                console.log(err);
+            } else{
+                if(allCampgrounds.length < 1) {
+	                    noMatch = "No Campgrounds match that search, please try again.";
+                        }
+            
+                res.render("campgrounds/index", {campgrounds: allCampgrounds, noMatch: noMatch});
+            }
+            
+        });
+    } else {
+        // get all campground for db
+        Campground.find({}, function(err, allCampgrounds){
+            if(err){
+                console.log(err);
+            } else{
+                res.render("campgrounds/index", {campgrounds: allCampgrounds, noMatch: noMatch});
+            }
+        });
+    }
     // res.render("campgrounds", {campgrounds: campgrounds}); //{name of object: data being passed}
 });
 
@@ -170,6 +188,11 @@ router.delete("/:id", middleware.checkCampgroundOwnership, function(req, res){
     
   
 // }
+
+
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
 
 
 module.exports = router;
